2b308058fff85b3ebce33ab0abc1c2111cfe3df2
Marcus Sjöblom <marsj402@student.liu.se>
2024-09-16 08:57:06 +0200

    Code cleanup
    Have changed variable names. Now the syntax for all nodes and variables are:
    
    hello_this_is_a_var
    
    All except for classnames!

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,7 @@ extends Node
 
 var score = 0
 
-@onready var hud: CanvasLayer = %HUD
+@onready var hud: CanvasLayer = %hud
 
 # Add a point for collected cheese and update HUD
 func add_point() -> void:
