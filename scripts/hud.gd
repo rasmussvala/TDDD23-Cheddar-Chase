e453759827d1b4f24c8e683416a03021e9bbc9bd
rasmussvala <rasmus.svala@hotmail.com>
2024-09-18 10:11:54 +0200

    Added bouncing heart when taking damage

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -11,11 +11,20 @@ var heart_empty: Texture2D = preload("res://assets/heart/heart-empty.png")
 func update_score(score) -> void:
 	score_label.text = str(score) + " Cheese"
 
-func update_health(health) -> void:
+func update_health(new_health) -> void:
 	var hearts = [heart_3, heart_2, heart_1]
 	
 	for i in range(hearts.size()):
-		if i < health:
+		if i < new_health:
 			hearts[i].texture = heart_filled
+		elif i == new_health:
+			hearts[i].texture = heart_empty
+			bounce_heart(hearts[i])
 		else: 
 			hearts[i].texture = heart_empty
+
+func bounce_heart(heart: Sprite2D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(heart, "scale", Vector2(10.0, 10.0), 0.15).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(heart, "scale", Vector2(8.0, 8.0), 0.15).set_trans(Tween.TRANS_BOUNCE)
+	#tween.tween_callback(heart.queue_free)
