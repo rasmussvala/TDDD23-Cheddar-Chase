af8237b1400b1d10e877fb0aaaebee892016668d
rasmussvala <rasmus.svala@hotmail.com>
2024-09-25 14:28:42 +0200

    Basic Cat Decision
    Cat has three states:
    * IDLE
    * EATING
    * SLEEPING

--- scripts/characters/enemies/cat.gd
+++ scripts/characters/enemies/cat.gd
@@ -1,25 +1,63 @@
 extends CharacterBody2D
 
+class_name Cat
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Cat's attributes
+var hunger: float = 0.0
+var tiredness: float = 0.0
 
+# Constants
+@export var MAX_HUNGER = 100.0
+@export var HUNGER_INCREASE_RATE = 5.0
+@export var HUNGER_THRESHOLD = 80
+@export	var EATING_RATE = 25 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@export var MAX_TIREDNESS = 100.0
+@export var TIREDNESS_INCREASE_RATE = 4.0
+@export var TIREDNESS_THRESHOLD = 95 
+@export var SLEEPING_RATE = 20
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+# States
+enum State {IDLE, EATING, SLEEPING}
+var current_state = State.IDLE
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _process(delta):
+	print("hunger: " + str("%.2f" % hunger) 
+	+ ", tiredness: " + str("%.2f" % tiredness) 
+	+ ", state: " + str(current_state))
+	
+	# Increase hunger and tiredness over time
+	hunger = min(hunger + HUNGER_INCREASE_RATE * delta, MAX_HUNGER)
+	tiredness = min(tiredness + TIREDNESS_INCREASE_RATE * delta, MAX_TIREDNESS)
+	
+	# Perform action based on current state
+	match current_state:
+		State.IDLE:
+			idle_action()
+		State.EATING:
+			eating_action(delta)
+		State.SLEEPING:
+			sleeping_action(delta)
+
+func make_decision():
+	if hunger >= HUNGER_THRESHOLD:
+		current_state = State.EATING
+	elif tiredness >= TIREDNESS_THRESHOLD:
+		current_state = State.SLEEPING
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		current_state = State.IDLE
 
-	move_and_slide()
+func idle_action():
+	make_decision()
+
+func eating_action(delta):
+	hunger -= EATING_RATE * delta
+	if hunger <= 0:
+		current_state = State.IDLE
+		make_decision()
+
+func sleeping_action(delta):
+	tiredness -= SLEEPING_RATE * delta
+	if tiredness <= 0:
+		current_state = State.IDLE
+		make_decision()
