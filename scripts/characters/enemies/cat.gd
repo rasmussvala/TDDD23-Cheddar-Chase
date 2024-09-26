7f7879d280235836974f98c366cf29e1cc5b849a
rasmussvala <rasmus.svala@hotmail.com>
2024-09-26 14:24:01 +0200

    Added walking state to cat
    Still WIP

--- scripts/characters/enemies/cat.gd
+++ scripts/characters/enemies/cat.gd
@@ -19,8 +19,10 @@ var tiredness: float = 0.0
 @export var TIREDNESS_THRESHOLD = 95 
 @export var SLEEPING_RATE = 20
 
+@export var MOVEMENT_SPEED = 100
+
 # States
-enum State {IDLE, EATING, SLEEPING}
+enum State {IDLE, MOVING_TO_FOOD, EATING, MOVING_TO_BED, SLEEPING}
 var current_state = State.IDLE
 
 func _process(delta):
@@ -36,18 +38,36 @@ func _process(delta):
 	match current_state:
 		State.IDLE:
 			idle_action()
+		State.MOVING_TO_FOOD:
+			move_to_target(Vector2(700.0, 300.0))
 		State.EATING:
 			eating_action(delta)
+		State.MOVING_TO_BED:
+			move_to_target(Vector2(400.0, 300.0))
 		State.SLEEPING:
 			sleeping_action(delta)
 
 func make_decision():
 	if hunger >= HUNGER_THRESHOLD:
-		current_state = State.EATING
+		current_state = State.MOVING_TO_FOOD
 	elif tiredness >= TIREDNESS_THRESHOLD:
-		current_state = State.SLEEPING
+		current_state = State.MOVING_TO_BED
 	else:
 		current_state = State.IDLE
+
+func move_to_target(target: Vector2):
+	animated_sprite_cat.play("walk")
+	var direction = (target - global_position).normalized()
+	velocity = direction * MOVEMENT_SPEED
+	animated_sprite_cat.rotation = velocity.angle()
+	move_and_slide()
+	
+	# Check if we've reached the target
+	if global_position.distance_to(target) < 1:  # ADJUST LATER
+		if current_state == State.MOVING_TO_FOOD:
+			current_state = State.EATING
+		elif current_state == State.MOVING_TO_BED:
+			current_state = State.SLEEPING
 
 func idle_action():
 	animated_sprite_cat.play("sit")
