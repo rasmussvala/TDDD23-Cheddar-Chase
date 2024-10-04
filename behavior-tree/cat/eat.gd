ecb77c202891c8f7be6f48ac34a06b1389571e43
rasmussvala <rasmus.svala@hotmail.com>
2024-10-04 12:28:19 +0200

    Cat eats with a specified rate

--- behavior-tree/cat/eat.gd
+++ behavior-tree/cat/eat.gd
@@ -2,6 +2,8 @@ extends BTAction
 
 func tick(blackboard: Dictionary) -> int:
 	if blackboard["hunger"] > 0:
-		blackboard["hunger"] = 0
-		return RUNNING # Doesn't do anything atm
-	return SUCCESS
+		blackboard["hunger"] -= blackboard["delta"] * blackboard["eating_rate"]
+		return RUNNING
+	else:
+		blackboard["is_eating"] = false
+		return SUCCESS
--- behavior-tree/nodes/bt_tree.gd
+++ behavior-tree/nodes/bt_tree.gd
@@ -3,23 +3,24 @@ class_name BTTree extends Node
 enum Status { SUCCESS, FAILURE, RUNNING }
 
 var root: BTSelector
-
 var blackboard: Dictionary = {
 	"hunger": 0.0,
 	"max_hunger": 100,
 	"hunger_threshold": 80,
 	"eating_rate": 10,
-	"hunger_increase_rate": 5
+	"hunger_increase_rate": 5,
+	"is_eating": false  
 }
 
 func _ready():
-	root = $behavior
+	root = $root
 
 func _process(delta):
+	blackboard["delta"] = delta
+	
 	if root:
 		root.tick(blackboard)
-	
-	# Increase hunger and tiredness over time
+	   
 	blackboard["hunger"] = min(blackboard["hunger"] + blackboard["hunger_increase_rate"] * delta, blackboard["max_hunger"])
 	
 	print(blackboard["hunger"])
