2fb05d445f91c8eaa71ef6b5c687855bc273c297
rassv453 <rassv453@student.liu.se>
2024-10-14 13:54:35 +0200

    Moved cat assets to separate folder

--- behavior-tree/cat/is_food_in_bowl.gd
+++ behavior-tree/cat/is_food_in_bowl.gd
@@ -4,7 +4,7 @@ func tick(blackboard: Dictionary) -> int:
 	var sprite = blackboard.get("food")
 	
 	# Food is in the bowl 
-	if sprite.texture == preload("res://assets/characters/enemies/cat/plate-food.png"):
+	if sprite.texture == preload("res://assets/cat/stuff/plate-food.png"):
 		return SUCCESS
 	# Bowl is empty
 	else:
