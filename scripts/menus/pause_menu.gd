0367d9eebbd6b6fd7f9ff2409f10b8c7d740a569
rassv453 <rassv453@student.liu.se>
2024-10-23 14:30:48 +0200

    Skeleton pause menu

---
+++ scripts/menus/pause_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _on_resume_button_pressed() -> void:
+	print("Resume")
+
+func _on_restart_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_menu_button_pressed() -> void:
+	pass # Replace with function body.
