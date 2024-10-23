cf9ba5f0dd612ead3855e263898e15800c35de78
rassv453 <rassv453@student.liu.se>
2024-10-23 15:10:30 +0200

    Player can pause game
    in level 1

--- scripts/menus/pause_menu.gd
+++ scripts/menus/pause_menu.gd
@@ -1,7 +1,21 @@
 extends CanvasLayer
 
+var is_paused: bool = false
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS # Make sure we can process this scene when paused
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):  # Escape key
+		toggle()
+
+func toggle() -> void:
+	is_paused = !is_paused
+	get_tree().paused = is_paused  # Pause the game
+	visible = is_paused   # Show/hide the pause menu
+
 func _on_resume_button_pressed() -> void:
-	print("Resume")
+	toggle()
 
 func _on_restart_button_pressed() -> void:
 	pass # Replace with function body.
