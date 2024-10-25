aa214c0c5520d10a0d0c5a8ef4d6c19c7b918d6a
rassv453 <rassv453@student.liu.se>
2024-10-25 09:44:54 +0200

    Added reset functionality to options menu

--- scripts/menus/options_menu.gd
+++ scripts/menus/options_menu.gd
@@ -1,4 +1,30 @@
 extends CanvasLayer
 
+@onready var you_sure_screen: ColorRect = %you_sure_screen
+@onready var data_deleted_screen: ColorRect = %data_deleted_screen
+
+func _ready() -> void:
+	you_sure_screen.visible = false
+	data_deleted_screen.visible = false
+
 func _on_reset_progress_button_pressed() -> void:
-	pass 
+	you_sure_screen.visible = true
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
+
+func _on_yes_button_pressed() -> void:
+	save_manager.delete_save()
+	save_manager.initialize_save()
+	game_data.current_level_icon_name = "1"
+	
+	data_deleted_screen.visible = true
+
+
+func _on_no_button_pressed() -> void:
+	you_sure_screen.visible = false
+
+
+func _on_ok_button_pressed() -> void:
+	data_deleted_screen.visible = false
+	you_sure_screen.visible = false
